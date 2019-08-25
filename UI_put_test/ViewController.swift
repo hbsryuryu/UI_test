61cbaf7f039a249934bc5430834b3146a2e9ba68
hbsryuryu <hbsryuryu@gmail.com>
2019-08-25 13:38:01 +0900

    put UI put button label IBAction and IBoutlet

--- UI_put_test/ViewController.swift
+++ UI_put_test/ViewController.swift
@@ -8,13 +8,22 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var button1: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func buttun(_ sender: Any) {
+        label1.text = "Pushed"
+    }
+    
 }
 
